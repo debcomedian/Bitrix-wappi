0189a05de7af274e80c6614099c0e5d17e439c91
debcomedian <vital.egorov2004@gmail.com>
2025-10-01 10:54:49 +0300

    fixed migrate db error

--- .last_version/1.1.0/install/version.php
+++ .last_version/1.1.0/install/version.php
@@ -1,5 +1,5 @@
 <?
 $arModuleVersion = array(
-	"VERSION" => "1.1.0",
-	"VERSION_DATE" => "2025-09-03"
+	"VERSION" => "1.1.1",
+	"VERSION_DATE" => "2025-09-05"
 );
--- .last_version/1.1.0/updater.php
+++ .last_version/1.1.0/updater.php
@@ -18,6 +18,17 @@ if ($updater->CanUpdateDatabase()) {
             $connection->queryExecute("ALTER TABLE $tableName CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
         }
     }
+    if ($updater->PreviousVersion < '1.1.0') {
+        $connection = Application::getConnection();
+        $tableName  = 'wappipro_template';
+
+        if ($connection->isTableExists($tableName)) {
+            $existsRes = $connection->query("SHOW COLUMNS FROM {$tableName} LIKE 'SITE_ID'");
+            if (!$existsRes->fetch()) {
+                $connection->queryExecute("ALTER TABLE {$tableName} ADD COLUMN SITE_ID CHAR(2) NULL");
+            }
+        }
+    }
 }
 
 ?>
